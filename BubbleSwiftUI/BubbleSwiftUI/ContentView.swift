2270a00d6b95233e1d73491d5b732169f5636ac0
JSKeum <JSKeum2512@gmail.com>
2019-11-13 21:06:58 +0900

    SwiftUI 프로젝트 따로 생성

---
+++ BubbleSwiftUI/BubbleSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BubbleSwiftUI
+//
+//  Created by JSKeum on 2019/11/13.
+//  Copyright © 2019 BubbleTalkTeam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
