28faca5bb20cc6d7581c03bc9586ea2b741b597f
JSKeum <JSKeum2512@gmail.com>
2019-11-13 21:00:19 +0900

    BubbleTalk 프로젝트 생성

---
+++ BubbleTalk/BubbleTalk/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BubbleTalk
+//
+//  Created by JSKeum on 2019/11/13.
+//  Copyright © 2019 BubbleTalkTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    // SwiftUI와 연결
+//    @IBSegueAction func addSwiftUIView(_ coder: NSCoder) -> UIViewController? {
+//           return UIHostingController(coder: coder, rootView: BubbleSwiftUIView())
+//       }
+
+    
+}
+
