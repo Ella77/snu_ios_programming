50dd1568856bc94549f6ef529b4f5c84d6d5629a
xxxdokki <xxxdokki@xxxdokkiui-MacBook.local>
2019-11-16 13:00:52 +0900

    xcode로 button만듦. swift ui와의 연결 필요

---
+++ BubbleTalk/BubbleTalk/SwiftUIModules/Bubbles.swift
@@ -0,0 +1,21 @@
+//
+//  Bubbles.swift
+//  BubbleTalk
+//
+//  Created by xxxdokki on 16.11.19.
+//  Copyright © 2019 BubbleTalkTeam. All rights reserved.
+//
+
+import SwiftUI
+
+struct Bubbles: View {
+    var body: some View {
+        Circle()
+    }
+}
+
+struct Bubbles_Previews: PreviewProvider {
+    static var previews: some View {
+        Bubbles()
+    }
+}
--- BubbleTalk/BubbleTalk/ViewController.swift
+++ BubbleTalk/BubbleTalk/ViewController.swift
@@ -8,17 +8,22 @@
 
 import UIKit
 import SwiftUI
-//ㅎㅎㅎㅎㅎㅎ
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    //var bubble: Bubbles
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func newBubble(_ sender: UIButton) {
+       // bubble.newbubbleButton()
+    }
     
     // SwiftUI와 Hosting 방식으로 연결
     @IBSegueAction func addSwiftUI(_ coder: NSCoder) -> UIViewController? {
-        return UIHostingController(coder: coder, rootView: BubbleSwiftUIView())
+        return UIHostingController(coder: coder, rootView: Bubbles())
     }
 }
 
