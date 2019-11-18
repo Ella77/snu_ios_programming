412986a668caf23fa66ecad36c24c3cd8c99262f
SiennaHan <soohyeon.0131@gmail.com>
2019-11-18 20:55:29 +0900

    read json

--- BubbleTalk/BubbleTalk/Models/Bubble.swift
+++ BubbleTalk/BubbleTalk/Models/Bubble.swift
@@ -13,9 +13,32 @@ struct Bubble: Hashable, Identifiable {
     var text: String
     var size: Int
     
+    /*
+     extension Bubble{
+     var toJSON: Any{
+        return [
+            "text": text,
+            "size": size
+        ]
+     }
+     
+     init?(fromJSON json: Any){
+        guard
+            let bubble = json as? [String: Any],
+            let text = bubble["text"] as? String,
+            let size = bubble["size"] as? Int
+            else{
+                print("Couldn't parse JSON")
+                return nil
+            }
+     }
+     */
+    
     init(text txt: String, size sz: Int, id bubId: Int) {
         text = txt
         size = sz
         id = bubId
     }
+
+    
 }
